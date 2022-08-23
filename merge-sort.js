d259ada4b51bdb40d4f17b66718cf22f485e56b0
James Rozsypal <jimmerxl@gmail.com>
2022-08-23 13:25:02 -0400

    Clean up mergesort and add a larger test case

--- merge-sort.js
+++ merge-sort.js
@@ -8,18 +8,14 @@ pseudocode:
 const merge = (left = [], right = []) => {
   const workingArr = []; // for storing sorted values
 
+  // keep sorting until left and right are empty
   while (left.length > 0 && right.length > 0) {
-    let minArr = []; // for storing the array with the lowest value
-
-    // sort the arrays in ascending order
+    // sort the arrays in ascending order by pushing the lowest value onto the working array
     if (left[0] < right[0]) {
-      minArr = left;
+      workingArr.push(left.shift());
     } else {
-      minArr = right;
+      workingArr.push(right.shift());
     }
-
-    // push the lowest value onto the sorted working array
-    workingArr.push(minArr.shift());
   }
 
   return workingArr.concat(left, right); // merge together the two sorted arrays
@@ -27,20 +23,24 @@ const merge = (left = [], right = []) => {
 
 const mergeSort = (arr = []) => {
   if (arr.length <= 1) return arr; // base case for small arrays that don't need sorting
+
   const middle = Number(arr.length / 2).toFixed(); // find the middle of the array for splitting
   const left = mergeSort(arr.slice(0, middle)); // sort the left half of the array
   const right = mergeSort(arr.slice(middle, arr.length)); // sort the right half of the array
+
   return merge(left, right); // merge the two halves together and return
 };
 
 const main = (() => {
   const arr6 = [9, 7, 8, 3, 2, 1];
   const arr10 = [42, 12, 7, 90, 8, 78, 43, 71, 21, 76];
-  console.log(
-    "Merge sort an array of size 6 ([9, 7, 8, 3, 2, 1]):  " + mergeSort(arr6)
-  );
-  console.log(
-    "Merge sort an array of size 10 ([42, 12, 7, 90, 8, 78, 43, 71, 21, 76]):  " +
-      mergeSort(arr10)
-  );
+  const arr47 = [
+    39, 58, 93, 10, 23, 87, 24, 55, 94, 38, 75, 80, 11, 43, 19, 47, 2, 32, 5,
+    79, 84, 3, 97, 43, 19, 88, 24, 30, 13, 56, 23, 27, 8, 65, 99, 69, 78, 97,
+    81, 49, 71, 27, 31, 23, 79, 44, 16,
+  ];
+
+  console.log("Merge sort an array of size 6:  " + mergeSort(arr6));
+  console.log("Merge sort an array of size 10:  " + mergeSort(arr10));
+  console.log("Merge sort an array of size 47:  " + mergeSort(arr47));
 })();
